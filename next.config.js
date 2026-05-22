75bf28476c67a7dda0729258958c5b71acc2e322
Hengky Yalandra <hengkyyalandra31051996@gmail.com>
2026-05-22 23:34:08 +0700

    fix: update next config serverExternalPackages

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ['unpdf'],
-  },
+  serverExternalPackages: ['unpdf'],
 }
 
 module.exports = nextConfig
